c4b6f376269e7cf61d4bf02678077f777b4486ea
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-07 18:23:43 -0800

    Add files to update env file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
-
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -19,7 +19,8 @@ module.exports = {
       title: 'template-repo',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
